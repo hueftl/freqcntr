8bd07ab1b443b67cc80d93e18e5c618b37c0452c
nextgenerationgeek <nextgenerationgeek@users.noreply.github.com>
2015-03-31 09:56:52 +0200

    Update counter.c
    minor changes

--- counter.c
+++ counter.c
@@ -18,41 +18,41 @@
 #include <util/delay.h>
 
 volatile int 
-	tick1 = 0;					// Zählvar.
+	tick1 = 0;			// Zählvar.
 
 volatile unsigned char			// Timer1 Overflows
 	count_overflows = 0;
 
 volatile uint32_t 
-	freq1 = 0,					// Var. 1s-Gate
-	freq_ausgabe = 0,			// 32-bit für Ergebnis
+	freq1 = 0,			// Var. 1s-Gate
+	freq_ausgabe = 0,		// 32-bit für Ergebnis
 	freq_ausgabe_high1 = 0;		// Var. für Ausgabe hohe Freq. (ab 50kHz)
 
 double korrekturwert = 0.999985750203;	// Korrekturwert ausgemessen mit externem 16MHz Quarz
 		
 /*** Timer Routinen ***/
-ISR(TIMER0_COMPA_vect)		// wird mit 125Hz  aufgerufen (16MHz/1024/125, alle 8ms)
+ISR(TIMER0_COMPA_vect)			// wird mit 125Hz  aufgerufen (16MHz/1024/125, alle 8ms)
 	{
-		uint8_t adcin_temp;		// Var. für PINF
-		uint16_t counter_temp;		// Var. zur zwischenspeicherung Zählerstand
+		uint8_t adcin_temp;	// Var. für PINF
+		uint16_t counter_temp;	// Var. zur zwischenspeicherung Zählerstand
 
 		adcin_temp = PINF;
-		counter_temp = TCNT1;		// Zählerwert in Variable lesen
+		counter_temp = TCNT1;	// Zählerwert in Variable lesen
 
 		tick1++;
-		if(tick1 == 125) // Torzeit: 1s, Sekundenbasis (16MHz/1024/125/125=1Hz)
+		if(tick1 == 125) 	// Torzeit: 1s, Sekundenbasis (16MHz/1024/125/125=1Hz)
 			{
-				PORTB |= (1<<PB0);		// 74HC393 clear
+				PORTB |= (1<<PB0);	// 74HC393 clear
 				asm volatile ("NOP");
 				asm volatile ("NOP");
-				PORTB &= ~(1<<PB0);		// 74HC393 auf Ursprung
-				TCNT1 = 0;				    // Zählerwert rücksetzen
-				tick1 = 0;			    	// Tick rücksetzen
+				PORTB &= ~(1<<PB0);	// 74HC393 auf Ursprung
+				TCNT1 = 0;		// Zählerwert rücksetzen
+				tick1 = 0;		// Tick rücksetzen
 				// Frequenzregister bilden, mit Korrekturwert
 				freq1 = ( (((uint32_t)count_overflows)<<24) | (((uint32_t)counter_temp)<<8) | (uint32_t)adcin_temp);
 				//freq_change=1;
-				adcin_temp = 0;			// Variable Port A rücksetzen
-				counter_temp = 0;		// Variable Zählerstand rücksetzen
+				adcin_temp = 0;		// Variable Port A rücksetzen
+				counter_temp = 0;	// Variable Zählerstand rücksetzen
 				count_overflows = 0;	// Timer1 OVF rücksetzen
 			}
 }
@@ -80,10 +80,14 @@ int main()
 		// Timer1: Flanken zählen
 		TCCR1A |= (1<<ICES1) | (1<<CS11);	// Prescale 8, Input Capture Edge Select (S.110)
 		TIMSK1 |= (1<<TOIE1) | (1<<ICIE1); 	// OVF und CAPT Int erlauben, Timer1 (S.112)
+		
+		//TO-DO: LCD-Init und Testausgabe bei Systemstart
 
 		sei();					// Interrupts erlauben
 
-		while(1){
-			freq_ausgabe_high1 = (freq1 * korrekturwert);	// Frequenz mit Korrektur berechnen
-		  }
+		while(1)
+			{
+				freq_ausgabe_high1 = (freq1 * korrekturwert);	// Frequenz mit Korrektur berechnen
+				//TO-DO: freq_ausgabe_high1 -> Auf LCD ausgeben
+		  	}
 	}
