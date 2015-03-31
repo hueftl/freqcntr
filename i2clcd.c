d76bb8bfb903917ccec05586112df4bf73b80071
nextgenerationgeek <nextgenerationgeek@users.noreply.github.com>
2015-03-31 10:15:43 +0200

    Update i2clcd.c

--- i2clcd.c
+++ i2clcd.c
@@ -1,1 +1,23 @@
+#include "i2clcd.h"
 
+// LCD Initialisierung
+void lcd_init(void)
+  {
+    
+  }
+
+// Auf I2C schreiben
+void lcd_write_i2c()
+  {
+
+  }
+
+// Wartemöglichkeit
+void lcd_wait_ms(unsigned short ms)
+  {
+	  unsigned short i;
+	  for(i = 0; i < ms; i++)
+	    {
+		    wait1ms;
+	    }
+  }
