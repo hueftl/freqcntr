b45e0dc77d4cf60ff20b1849c75699c428eccdc2
Alexander Menne <alexander.menne@nextgenerationgeek.de>
2017-03-06 22:00:58 +0100

    minor addendum

---
+++ src/timer0.rs
@@ -0,0 +1,23 @@
+
+pub enum ClockSource {
+    None,
+    Prescaler1,
+    Prescaler8,
+    Prescaler64,
+    Prescaler256,
+    Prescaler1024,
+    ExternalFalling,
+    ExternalRising,
+}
+
+impl ClockSource {
+
+}
+
+pub struct Timer {
+
+}
+
+impl Timer {
+
+}
